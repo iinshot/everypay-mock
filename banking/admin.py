e99e2de09cb7c5c9a23296cc6bc5dfcadb7ae2a6
ernest <ernest_s@bk.ru>
2026-06-11 11:20:40 +0330

    remove Balance model

--- banking/admin.py
+++ banking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ThirdParty, Company, Account, Balance, StatementRequest, Transaction
+from .models import ThirdParty, Company, Account, StatementRequest, Transaction
 
 class StatementInline(admin.TabularInline):
     model = StatementRequest
@@ -17,11 +17,6 @@ class TransactionInline(admin.TabularInline):
     )
     show_change_link = True
 
-class BalanceInline(admin.TabularInline):
-    model = Balance
-    extra = 1
-    fields = ("amount", "currency", "credit_debit_indicator", "type", "datetime")
-
 @admin.register(ThirdParty)
 class ThirdPartyAdmin(admin.ModelAdmin):
     list_display = ("type", "code", "name", "description")
@@ -38,13 +33,12 @@ class AccountAdmin(admin.ModelAdmin):
     list_filter = ("status", "account_type", "bank")
     search_fields = ("bban", "company__name")
     readonly_fields = ("account_id",)
-    inlines = [BalanceInline, StatementInline]
+    inlines = [TransactionInline, StatementInline]
 
 @admin.register(StatementRequest)
 class StatementRequestAdmin(admin.ModelAdmin):
     list_display = ("statement_id", "account", "created_at", "status_display")
     readonly_fields = ("statement_id", "created_at")
-    inlines = [TransactionInline]
 
     def status_display(self, obj):
         return obj.status
@@ -54,7 +48,7 @@ class StatementRequestAdmin(admin.ModelAdmin):
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = (
-        "booking_date_time", "statement", "credit_debit_indicator",
+        "booking_date_time", "account", "credit_debit_indicator",
         "amount", "currency", "status"
     )
     list_filter = ("credit_debit_indicator", "status")
@@ -62,7 +56,7 @@ class TransactionAdmin(admin.ModelAdmin):
     readonly_fields = ("transaction_id",)
     fieldsets = (
         ("Основное", {
-            "fields": ("transaction_id", "statement", "credit_debit_indicator", "status"),
+            "fields": ("transaction_id", "account", "credit_debit_indicator", "status"),
         }),
         ("Сумма и дата", {
             "fields": ("amount", "currency", "booking_date_time", "value_date_time", "document_number", "description"),
